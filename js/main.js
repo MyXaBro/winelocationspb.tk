eedd8394dfdd618b50917e149db3c64e970b8b62
MyXaBro <65068600+MyXaBro@users.noreply.github.com>
2020-08-27 19:16:21 +0300

    ref
    ....

--- js/main.js
+++ js/main.js
@@ -229,7 +229,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "sendEmail.php",
+		      url: "inc/sendEmail.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
