27fcd5fc3bae4c1a29824aa397287eda373dea87
MyXaBro <65068600+MyXaBro@users.noreply.github.com>
2020-08-27 18:13:19 +0300

    original
    ...

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,7 +1,7 @@
 ﻿<?php
 
 // Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
+$siteOwnersEmail = 'winelocation@cloud.com';
 
 
 if($_POST) {
@@ -13,15 +13,15 @@ if($_POST) {
 
    // Check Name
 	if (strlen($name) < 2) {
-		$error['name'] = "Please enter your name.";
+		$error['name'] = "Пожалуйста, введите ваше имя.";
 	}
 	// Check Email
 	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-		$error['email'] = "Please enter a valid email address.";
+		$error['email'] = "Пожалуйста, введите ваш email адрес.";
 	}
 	// Check Message
 	if (strlen($contact_message) < 15) {
-		$error['message'] = "Please enter your message. It should have at least 15 characters.";
+		$error['message'] = "Пожалуйста, введите ваше сообщение, не менее 15-ти символов";
 	}
    // Subject
 	if ($subject == '') { $subject = "Contact Form Submission"; }
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo "Что-то пошло не так, попробуйте ещё раз."; }
 		
 	} # end if - no validation error
 
